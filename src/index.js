74f0b3a0e44dd630ff3e19f42c8f0cf45f30bcdc
Psychotheus <psychotheus@gmail.com>
2018-09-12 00:23:05 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let rules = [].concat(...bracketsConfig);
+
+  let st = [];
+  let index = 0;
+  let same = false;
+  //АЛГОРИТМ
+  //если скобка открывающая -> добавим в стек
+  //иначе -> eсли текущая скобка не закрывает предыдущую - строка некорректна
+  // + проверка на одинаковые скобки
+  for (let i = 0; i < str.length; i++) {
+    index = rules.indexOf(str[i]);
+    same = (rules[index] == rules[index + 1]);
+    
+    //проверка на одинаковые скобки
+    if (same) {
+      if (!st.includes(str[i])) {
+        st.push(str[i]);
+        continue;
+      } else if (str[i] == st[st.length - 1]) {
+        st.pop();
+        continue;
+      } else {
+        return false;
+      }
+
+    }
+
+    if (index % 2 == 0) {
+
+      st.push(str[i]);
+
+    } else if(st[st.length - 1] == rules[index - 1]) {
+
+      st.pop();
+
+    } else return false;
+  }
+
+  return (st.length == 0);
 }
